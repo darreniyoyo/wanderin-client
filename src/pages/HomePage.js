b63256835cee729b9335f58c80875e2d108632db
Darren Iyoyo <darreniyoyo@gmail.com>
2022-08-12 10:32:34 +0200

    class -> ClassName

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,17 +6,17 @@ function HomePage() {
      <head>
       <link href="HomePage.css" rel="stylesheet"></link>
       </head>   
-      <body class="d-flex h-100 text-center text-white bg-dark">
+      <body className="d-flex h-100 text-center text-white bg-dark">
 
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+<div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
 
-  <main class="px-3">
+  <main className="px-3">
     <h1>Welcome to Wanderin</h1>
-    <p class="lead">Wanderin is your go to tool to organise your wishlist</p>
+    <p className="lead">Wanderin is your go to tool to organise your wishlist</p>
   </main>
 
-  <footer class="mt-auto text-white-50">
+  <footer className="mt-auto text-white-50">
     <p>Website by Darren Iyoyo - 2022</p>
   </footer>
 </div>
